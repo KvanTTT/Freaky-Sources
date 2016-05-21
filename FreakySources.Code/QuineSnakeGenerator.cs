f3cf5699b9647d33cfd9e22f1f7903631028704a
Ivan Kochurkin <kvanttt@gmail.com>
2016-05-22 00:27:01 +0300

    Added final version of QuineSnake.

--- FreakySources.Code/QuineSnakeGenerator.cs
+++ FreakySources.Code/QuineSnakeGenerator.cs
@@ -9,63 +9,68 @@ namespace FreakySources.Code
     public class QuineSnakeGenerator
     {
         /*$ Input */
-        public static int 
+        public static int
             FieldWidth,
             FieldHeight,
+            FieldWidth2,
+            FieldHeight2,
             HeadRow,
             HeadColumn,
             FoodRow,
-            FoodColumn;
-        static Direction NewDir;
-        static Direction[] Dirs;
+            FoodColumn,
+            GameState;
+        static int NewDir;
+        static int[] Dirs;
         static string R;
 
         public static string NewDirString
         {
-            get { return "Direction." + NewDir.ToString(); }
+            get { return "" + NewDir.ToString(); }
         }
         public static string DirsString
         {
-            get { return "{" + string.Join(",", Dirs.Select(d => "Direction." + d)) + "}"; }
+            get { return "{" + string.Join(",", Dirs.Select(d => "" + d)) + "}"; }
         }
         /* Input $*/
 
         static QuineSnakeGenerator()
         {
             FieldWidth = 20;
-            FieldHeight = 15;
-            var FieldState = new ItemState[FieldHeight, FieldWidth];
-            HeadRow = FieldHeight / 2;
+            FieldHeight = 10;
+            FieldWidth2 = FieldWidth / 2;
+            FieldHeight2 = FieldHeight / 2;
+            var FieldState = new int[FieldHeight, FieldWidth];
+            HeadRow = FieldHeight / 2 - 1;
             HeadColumn = FieldWidth / 2;
-            NewDir = Direction.Bottom;
-            Dirs = new[] { Direction.Bottom, Direction.Bottom };
+            NewDir = Bottom;
+            Dirs = new[] { Bottom, Bottom };
 
             int i, y = HeadRow, x = HeadColumn;
-            Direction dir = Direction.Left;
-            FieldState[y, x] = ItemState.Head;
+            int dir = Left;
+            FieldState[y, x] = Head;
             for (i = 0; i < Dirs.Length - 1; i++)
             {
                 dir = Dirs[i];
-                if (dir == Direction.Left)
+                if (dir == Left)
                     x++;
-                if (dir == Direction.Right)
+                if (dir == Right)
                     x--;
-                if (dir == Direction.Top)
+                if (dir == Top)
                     y++;
-                if (dir == Direction.Bottom)
+                if (dir == Bottom)
                     y--;
-                FieldState[y, x] = dir == Direction.Left || dir == Direction.Right ? ItemState.Horizontal : ItemState.Vertical;
+                FieldState[y, x] = dir == Left || dir == Right ? Horizontal : Vertical;
             }
-            FieldState[y, x] = dir == Direction.Left || dir == Direction.Right ? ItemState.HorizontalTail : ItemState.VerticalTail;
+            FieldState[y, x] = dir == Left || dir == Right ? HorizontalTail : VerticalTail;
 
             var rand = new Random();
             for (; ; )
             {
                 FoodRow = rand.Next(FieldHeight);
                 FoodColumn = rand.Next(FieldWidth);
-                if (FieldState[FoodRow, FoodColumn] == ItemState.Empty)
+                if (FieldState[FoodRow, FoodColumn] == Empty)
                 {
-                    FieldState[FoodRow, FoodColumn] = ItemState.Food;
+                    FieldState[FoodRow, FoodColumn] = Food;
                     break;
                 }
             }
@@ -73,64 +78,64 @@ namespace FreakySources.Code
 
         /*#Snake*/
 
-        enum ItemState
-        {
-            Empty = 0,
-            Vertical = 1,
-            Horizontal = 2,
-            TopLeftBottomRightBend = 3,
-            TopRightBottomLeftBend = 4,
-            Head = 5,
-            VerticalTail = 6,
-            HorizontalTail = 7,
-            Food = 8
-        }
+        const int Empty = 0;
+        const int Vertical = 1;
+        const int Horizontal = 2;
+        const int TopLeftBottomRightBend = 3;
+        const int TopRightBottomLeftBend = 4;
+        const int Head = 5;
+        const int VerticalTail = 6;
+        const int HorizontalTail = 7;
+        const int Food = 8;
 
-        enum Direction
-        {
-            Left = 0,
-            Top = 1,
-            Right = 2,
-            Bottom = 3
-        }
+        const int Left = 0;
+        const int Top = 1;
+        const int Right = 2;
+        const int Bottom = 3;
 
-        static void MakeStepAndPrint()
+        const int Init = 0;
+        const int Playing = 1;
+        const int GameOver = 2;
+        const int Win = 3;
+
+        static string MakeStepAndPrint()
         {
-            int y = HeadRow, x = HeadColumn;
-            int prevY = y, prevX = x;
-            Direction dir = Direction.Left;
-            var FieldState = new ItemState[FieldHeight, FieldWidth];
-            Direction headDir = Direction.Bottom;
-            int offset = 0;
-            FieldState[FoodRow, FoodColumn] = ItemState.Food;
+            int y = HeadRow,
+                x = HeadColumn,
+                dir = Left,
+                headDir = Bottom,
+                offset = 0,
+                I1, I2, S, A1, A2, B1, B2,
+                l = Dirs.Length;
             bool expand = false;
 
-            int I1, I2;
-            ItemState S;
-            Direction A1, A2, B1, B2;
+            var FieldState = new int[FieldHeight, FieldWidth];
+            FieldState[FoodRow, FoodColumn] = Food;
+            if (GameState != Playing)
+                goto X;
 
-            for (int i = 0; i < Dirs.Length; i++)
+            for (int i = 0; i < l; i++)
             {
                 dir = Dirs[i];
 
                 var d = i == 0 ? NewDir : Dirs[i - 1];
-                if (d == Direction.Left || d == Direction.Right)
+                if (d == Left || d == Right)
                 {
-                    A1 = Direction.Left;
-                    A2 = Direction.Right;
-                    B1 = Direction.Top;
-                    B2 = Direction.Bottom;
-                    S = ItemState.Horizontal;
+                    A1 = Left;
+                    A2 = Right;
+                    B1 = Top;
+                    B2 = Bottom;
+                    S = Horizontal;
                     I1 = 0;
                     I2 = 1;
                 }
                 else
                 {
-                    A1 = Direction.Top;
-                    A2 = Direction.Bottom;
-                    B1 = Direction.Left;
-                    B2 = Direction.Right;
-                    S = ItemState.Vertical;
+                    A1 = Top;
+                    A2 = Bottom;
+                    B1 = Left;
+                    B2 = Right;
+                    S = Vertical;
                     I1 = 1;
                     I2 = 0;
                 }
@@ -147,55 +152,60 @@ namespace FreakySources.Code
 
                     if (HeadRow < 0 || HeadRow >= FieldHeight || HeadColumn < 0 || HeadColumn >= FieldWidth)
                     {
-                        //GameOver = true;
-                        return;
+                        GameState = GameOver;
+                        goto X;
                     }
-                    if (FieldState[HeadRow, HeadColumn] == ItemState.Food)
+                    if (FieldState[HeadRow, HeadColumn] == Food)
                         expand = true;
-                    FieldState[HeadRow, HeadColumn] = ItemState.Head;
+                    FieldState[HeadRow, HeadColumn] = Head;
                 }
 
-                if (i != Dirs.Length - 1 || expand)
-                    FieldState[y, x] = dir == B1 || dir == B2 ? dir == B1 ^ d != A1 ? ItemState.TopLeftBottomRightBend : ItemState.TopRightBottomLeftBend : S;
+                if (i != l - 1 || expand)
+                    FieldState[y, x] = dir == B1 || dir == B2 ? dir == B1 ^ d != A1 ? TopLeftBottomRightBend : TopRightBottomLeftBend : S;
                 else
-                    FieldState[y, x] = d == Direction.Left || d == Direction.Right ? ItemState.HorizontalTail : ItemState.VerticalTail;
+                    FieldState[y, x] = d == Left || d == Right ? HorizontalTail : VerticalTail;
 
-                prevY = y;
-                prevX = x;
-                if (dir == Direction.Left)
+                if (dir == Left)
                     x++;
-                if (dir == Direction.Right)
+                if (dir == Right)
                     x--;
-                if (dir == Direction.Top)
+                if (dir == Top)
                     y++;
-                if (dir == Direction.Bottom)
+                if (dir == Bottom)
                     y--;
             }
 
-            if (FieldState[HeadRow, HeadColumn] != ItemState.Head)
+            if (FieldState[HeadRow, HeadColumn] != Head)
             {
-                //GameOver = true;
-                return;
+                GameState = GameOver;
+                goto X;
             }
 
-            var l = Dirs.Length;
             if (expand)
             {
-                FieldState[y, x] = dir == Direction.Left || dir == Direction.Right ? ItemState.HorizontalTail : ItemState.VerticalTail;
+                FieldState[y, x] = dir == Left || dir == Right ? HorizontalTail : VerticalTail;
+
+                GameState = Win;
+                for (I1 = 0; I1 < FieldHeight; I1++)
+                    for (I2 = 0; I2 < FieldWidth; I2++)
+                        if (FieldState[I1, I2] == Empty)
+                            GameState = Playing;
+                if (GameState == Win)
+                    goto X;
 
                 var rand = new Random();
                 for (; ; )
                 {
                     FoodRow = rand.Next(FieldHeight);
                     FoodColumn = rand.Next(FieldWidth);
-                    if (FieldState[FoodRow, FoodColumn] == ItemState.Empty)
+                    if (FieldState[FoodRow, FoodColumn] == Empty)
                     {
-                        FieldState[FoodRow, FoodColumn] = ItemState.Food;
+                        FieldState[FoodRow, FoodColumn] = Food;
                         break;
                     }
                 }
 
-                var ar = new Direction[l + 1];
+                var ar = new int[l + 1];
                 for (offset = 0; offset < l; offset++)
                     ar[offset + 1] = Dirs[offset];
                 ar[0] = headDir;
@@ -208,24 +218,31 @@ namespace FreakySources.Code
                 Dirs[0] = headDir;
             }
 
-
-
-            R = "";
+            X:
+            var R = "";
             var rn = "\r\n";
             var s = new string('/', FieldWidth + 4);
             R += rn + s + rn;
 
             var stateChars = " |-\\/O\"=*";
-
             for (I1 = 0; I1 < FieldHeight; I1++)
             {
                 R += "//";
-                for (I2 = 0; I2 < FieldWidth; I2++)
-                    R += stateChars[(int)FieldState[I1, I2]].ToString();
-                R += "//" + rn;
+                if (GameState == Playing)
+                    for (I2 = 0; I2 < FieldWidth; I2++)
+                        R += stateChars[FieldState[I1, I2]].ToString();
+                else
+                {
+                    var m = GameState == GameOver ? "Game Over!" : "  Win!!!  ";
+                    int fw2 = FieldWidth / 2;
+                    R += new string(' ', fw2 - 5) + m + new string(' ', FieldWidth - fw2 - 5);
+                }
+                R += "//"; R += rn;
             }
 
-            R += s + rn;
+            R += s + "   QuineSnake by KvanTTT for GoStash" + rn;
+
+            return R;
         }
 
         /*Snake#*/
--- Patterns and Data/QuineSnake.cs
+++ Patterns and Data/QuineSnake.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Text;
-using C = System.Console;
 
 namespace QuineClock3
 {
@@ -13,18 +11,23 @@ namespace QuineClock3
             HeadRow = /*$HeadRow*/FieldHeight / 2/*HeadRow$*/,
             HeadColumn = /*$HeadColumn*/FieldWidth / 2/*HeadColumn$*/,
             FoodRow = /*$FoodRow*/0/*FoodRow$*/,
-            FoodColumn = /*$FoodColumn*/0/*FoodColumn$*/;
-        static Direction NewDir = /*$NewDir*/Direction.Bottom/*NewDir$*/;
-        static Direction[] Dirs = /*$Dirs*/ { Direction.Bottom, Direction.Bottom } /*Dirs$*/ ;
-        static string R;
+            FoodColumn = /*$FoodColumn*/0/*FoodColumn$*/,
+            NewDir = /*$NewDir*/3/*NewDir$*/,
+            GameState = /*$GameState*/0/*GameState$*/;
+        static int[] Dirs = /*$Dirs*/ { 3, 3 } /*Dirs$*/ ;
 
         /*#Snake*//*Snake#*/
 
         static void Main()
         {
-            int key = (int)C.ReadKey(true).Key;
-            NewDir = key < 37 || key > 40 ? Dirs[0] : key - 37;
-            MakeStepAndPrint();
+            if (GameState != 0)
+            {
+                int k = (int)Console.ReadKey(true).Key;
+                NewDir = k < 37 || k > 40 ? Dirs[0] : k - 37;
+            }
+            if (GameState == 0)
+                GameState = 1;
+            var R = MakeStepAndPrint();
             /*@*/
         }
     }
