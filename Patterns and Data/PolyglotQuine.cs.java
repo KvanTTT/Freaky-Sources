9a810d5f4317f29019d58ba6b01f32ab70be88a7
Ivan Kochurkin <kvanttt@gmail.com>
2016-09-07 15:46:13 +0300

    PolyglotQuine.cs.java size reduced.

--- Patterns and Data/PolyglotQuine.cs.java
+++ Patterns and Data/PolyglotQuine.cs.java
@@ -2,8 +2,8 @@
 using System;//\u002A\u002F
 class Program{public static void//\u000A\u002F\u002A
 Main//\u002A\u002Fmain
-(String[]args){String s="//#u000A#u002F#u002A^using System;//#u002A#u002F^class Program{public static void//#u000A#u002F#u002A^Main//#u002A#u002Fmain^(String[]args){String s=!$!,t=s;String[]a=new String[]{!!+(char)33,!!+(char)94,!!+(char)35,!!+(char)36,!#!!,!#n!,!##!,s};for(int i=0;i<4;i++)t=t.//#u000A#u002F#u002A^Replace//#u002A#u002Freplace^(a[i],a[i+4]);//#u000A#u002F#u002A^Console.Write//#u002A#u002FSystem.out.printf^(t);}}",t=s;String[]a=new String[]{""+(char)33,""+(char)94,""+(char)35,""+(char)36,"\"","\n","\\",s};for(int i=0;i<4;i++)t=t.//\u000A\u002F\u002A
+(String[]args){String s="//#u000A#u002F#u002A^using System;//#u002A#u002F^class Program{public static void//#u000A#u002F#u002A^Main//#u002A#u002Fmain^(String[]args){String s=!$!,t=s;int[]a=new int[]{33,94,35,36};String[]b=new String[]{!#!!,!#n!,!##!,s};for(int i=0;i<4;i++)t=t.//#u000A#u002F#u002A^Replace//#u002A#u002Freplace^(!!+(char)a[i],b[i]);//#u000A#u002F#u002A^Console.Write//#u002A#u002FSystem.out.printf^(t);}}",t=s;int[]a=new int[]{33,94,35,36};String[]b=new String[]{"\"","\n","\\",s};for(int i=0;i<4;i++)t=t.//\u000A\u002F\u002A
 Replace//\u002A\u002Freplace
-(a[i],a[i+4]);//\u000A\u002F\u002A
+(""+(char)a[i],b[i]);//\u000A\u002F\u002A
 Console.Write//\u002A\u002FSystem.out.printf
 (t);}}
