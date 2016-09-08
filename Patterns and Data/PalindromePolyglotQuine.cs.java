6d36a7a2b2852ac5c5a8fcc228da50e8aad2c01f
Ivan Kochurkin <kvanttt@gmail.com>
2016-09-09 02:38:01 +0300

    PolyglotQuine.cs.java and PalindromePolyglotQuine.cs.java size reduced.

--- Patterns and Data/PalindromePolyglotQuine.cs.java
+++ Patterns and Data/PalindromePolyglotQuine.cs.java
@@ -2,23 +2,19 @@
 using System;//\u002A\u002F
 class Program{public static void//\u000A\u002F\u002A
 Main//\u002A\u002Fmain
-(String[]args){int[]a=new int[]{33,94,35,64},b=new int[]{34,10,92,47};String s="@**@@@#u000A#u002F#u002A^using System;@@#u002A#u002F^class Program{public static void@@#u000A#u002F#u002A^Main@@#u002A#u002Fmain^(String[]args){int[]a=new int[]{33,94,35,64},b=new int[]{34,10,92,47};String s=!$!,t=s;for(int i=0;i<4;i++)t=t.@@#u000A#u002F#u002A^Replace@@#u002A#u002Freplace^((char)a[i],(char)b[i]);t=t.@@#u000A#u002F#u002A^Replace@@#u002A#u002Freplace^(!!+(char)36,s);@@#u000A#u002F#u002A^var r=t.ToCharArray();Array.Reverse(r);t=t+'*'+new string(r);@@#u002A#u002Ft=t+'*'+new StringBuilder(t).reverse().toString();^@@#u000A#u002F#u002A^Console.Write@@#u002A#u002FSystem.out.printf^(t);}}/",t=s;for(int i=0;i<4;i++)t=t.//\u000A\u002F\u002A
+(String[]args){String s="`**?`@#_^using System;?_#^class Program{public static void?@#_^Main?_#main^(String[]args){String s=!$!,t=s;int[]a=new int[]{33,94,38,64,35,95,96,63,36};String[]b=new String[]{!&!!,!&n!,!&&!,!&@!,!&#!,!&_!,!`!,!?!,s};for(int i=0;i<9;i++)t=t.?@#_^Replace?_#replace^(!!+(char)a[i],b[i]);?@#_^var r=t.ToCharArray();Array.Reverse(r);t=t+'*'+new String(r);?_#t=t+'*'+new StringBuilder(t).reverse().toString();^?@#_^Console.Write?_#System.out.printf^(t);}}/",t=s;int[]a=new int[]{33,94,38,64,35,95,96,63,36};String[]b=new String[]{"\"","\n","\\","\\u000A","\\u002F","\\u002A","/","//",s};for(int i=0;i<9;i++)t=t.//\u000A\u002F\u002A
 Replace//\u002A\u002Freplace
-((char)a[i],(char)b[i]);t=t.//\u000A\u002F\u002A
-Replace//\u002A\u002Freplace
-(""+(char)36,s);//\u000A\u002F\u002A
-var r=t.ToCharArray();Array.Reverse(r);t=t+'*'+new string(r);//\u002A\u002Ft=t+'*'+new StringBuilder(t).reverse().toString();
+(""+(char)a[i],b[i]);//\u000A\u002F\u002A
+var r=t.ToCharArray();Array.Reverse(r);t=t+'*'+new String(r);//\u002A\u002Ft=t+'*'+new StringBuilder(t).reverse().toString();
 //\u000A\u002F\u002A
 Console.Write//\u002A\u002FSystem.out.printf
 (t);}}/*/}};)t(
 ftnirp.tuo.metsySF200u\A200u\//etirW.elosnoC
 A200u\F200u\A000u\//
-;)(gnirtSot.)(esrever.)t(redliuBgnirtS wen+'*'+t=tF200u\A200u\//;)r(gnirts wen+'*'+t=t;)r(esreveR.yarrA;)(yarrArahCoT.t=r rav
-A200u\F200u\A000u\//;)s,63)rahc(+""(
+;)(gnirtSot.)(esrever.)t(redliuBgnirtS wen+'*'+t=tF200u\A200u\//;)r(gnirtS wen+'*'+t=t;)r(esreveR.yarrA;)(yarrArahCoT.t=r rav
+A200u\F200u\A000u\//;)]i[b,]i[a)rahc(+""(
 ecalperF200u\A200u\//ecalpeR
-A200u\F200u\A000u\//.t=t;)]i[b)rahc(,]i[a)rahc((
-ecalperF200u\A200u\//ecalpeR
-A200u\F200u\A000u\//.t=t)++i;4<i;0=i tni(rof;s=t,"/}};)t(^ftnirp.tuo.metsySF200u#A200u#@@etirW.elosnoC^A200u#F200u#A000u#@@^;)(gnirtSot.)(esrever.)t(redliuBgnirtS wen+'*'+t=tF200u#A200u#@@;)r(gnirts wen+'*'+t=t;)r(esreveR.yarrA;)(yarrArahCoT.t=r rav^A200u#F200u#A000u#@@;)s,63)rahc(+!!(^ecalperF200u#A200u#@@ecalpeR^A200u#F200u#A000u#@@.t=t;)]i[b)rahc(,]i[a)rahc((^ecalperF200u#A200u#@@ecalpeR^A200u#F200u#A000u#@@.t=t)++i;4<i;0=i tni(rof;s=t,!$!=s gnirtS;}74,29,01,43{][tni wen=b,}46,53,49,33{][tni wen=a][tni{)sgra][gnirtS(^niamF200u#A200u#@@niaM^A200u#F200u#A000u#@@diov citats cilbup{margorP ssalc^F200u#A200u#@@;metsyS gnisu^A200u#F200u#A000u#@@@**@"=s gnirtS;}74,29,01,43{][tni wen=b,}46,53,49,33{][tni wen=a][tni{)sgra][gnirtS(
+A200u\F200u\A000u\//.t=t)++i;9<i;0=i tni(rof;}s,"//","/","A200u\\","F200u\\","A000u\\","\\","n\",""\"{][gnirtS wen=b][gnirtS;}63,36,69,59,53,46,83,49,33{][tni wen=a][tni;s=t,"/}};)t(^ftnirp.tuo.metsyS#_?etirW.elosnoC^_#@?^;)(gnirtSot.)(esrever.)t(redliuBgnirtS wen+'*'+t=t#_?;)r(gnirtS wen+'*'+t=t;)r(esreveR.yarrA;)(yarrArahCoT.t=r rav^_#@?;)]i[b,]i[a)rahc(+!!(^ecalper#_?ecalpeR^_#@?.t=t)++i;9<i;0=i tni(rof;}s,!?!,!`!,!_&!,!#&!,!@&!,!&&!,!n&!,!!&!{][gnirtS wen=b][gnirtS;}63,36,69,59,53,46,83,49,33{][tni wen=a][tni;s=t,!$!=s gnirtS{)sgra][gnirtS(^niam#_?niaM^_#@?diov citats cilbup{margorP ssalc^#_?;metsyS gnisu^_#@`?**`"=s gnirtS{)sgra][gnirtS(
 niamF200u\A200u\//niaM
 A200u\F200u\A000u\//diov citats cilbup{margorP ssalc
 F200u\A200u\//;metsyS gnisu
